e811ed2a6db403ec9bf659e004c1042abdcda743
Eddie Kim <cranklin@gmail.com>
2021-05-25 17:08:12 -0700

    Add Amazon Managed Blockchain support

---
+++ ethereumetl/providers/ambhttp.py
@@ -0,0 +1,47 @@
+import logging
+from typing import Any
+
+import os
+import requests
+from eth_typing import URI
+from web3._utils.request import _get_session
+from web3.providers.rpc import HTTPProvider
+from web3.types import Middleware, RPCEndpoint, RPCResponse
+from requests_auth_aws_sigv4 import AWSSigV4
+
+aws_auth = AWSSigV4(
+    'managedblockchain',
+    aws_access_key_id=os.environ.get('AWS_ACCESS_KEY_ID'),
+    aws_secret_access_key=os.environ.get('AWS_SECRET_ACCESS_KEY'),
+    region=AWS_REGION # us-east-1
+)
+
+
+def make_post_request(endpoint_uri: URI, data: bytes, *args: Any, **kwargs: Any) -> bytes:
+    kwargs.setdefault('timeout', 10)
+    session = _get_session(endpoint_uri)
+    # https://github.com/python/mypy/issues/2582
+    response = session.post(endpoint_uri, data=data,
+                            *args, **kwargs, auth=aws_auth)  # type: ignore
+    response.raise_for_status()
+
+    return response.content
+
+
+class AMBHTTPProvider(HTTPProvider):
+    def make_request(self, method: RPCEndpoint, params: Any) -> RPCResponse:
+        self.logger.debug("Making request HTTP. URI: %s, Method: %s",
+                          self.endpoint_uri, method)
+
+        # .decode() since the AWS sig library expects a string.
+        request_data = self.encode_rpc_request(method, params).decode()
+        raw_response = make_post_request(
+            self.endpoint_uri,
+            request_data,
+            **self.get_request_kwargs()
+        )
+        response = self.decode_rpc_response(raw_response)
+        self.logger.debug("Getting response HTTP. URI: %s, "
+                          "Method: %s, Response: %s",
+                          self.endpoint_uri, method, response)
+        return response
